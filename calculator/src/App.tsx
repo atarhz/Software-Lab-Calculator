29f29d6788163f381a2b7e6143595c60278beb79
a.rahimzadeh <a.rahimzadeh@emofid.com>
2023-10-30 12:39:40 +0330

    Some debug changes to show the calculator

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import './App.css';
+import Calculator from './components/calculator';
+import { LightModeProvider } from './components/lightModeContext';
+
 export default function App() {
-  // return (
-  //   <div className="App">
-      
-  //   </div>
-  // );
+  return (
+    <div className="App">
+      <LightModeProvider>
+        <Calculator />
+      </LightModeProvider>
+    </div>
+  );
 }
